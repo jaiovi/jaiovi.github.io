0e4539f4079d4b61c4a48f981db10c385e9a66e2
jaiovi <sebastianjo2011@gmail.com>
2026-03-22 18:33:07 +0100

    config ci cd

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jaiovi.github.io',
+  base: '/jaiovi.github.io',
   fonts: [{
     provider: fontProviders.fontsource(),
     name: "Outfit",
